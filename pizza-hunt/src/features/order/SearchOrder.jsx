3b89f73efef7a872d06d5a694c3c8db51fb5472f
Ankit Karnwal <karnwalankit89@gmail.com>
2024-05-30 01:39:54 +0530

    Final

--- pizza-hunt/src/features/order/SearchOrder.jsx
+++ pizza-hunt/src/features/order/SearchOrder.jsx
@@ -1,14 +1,14 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function SearchOrder() {
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState('');
   const navigate = useNavigate();
   function handleSubmit(event) {
     event.preventDefault();
     if (!query) return;
     navigate(`/order/${query}`);
-    setQuery("");
+    setQuery('');
   }
   return (
     <form onSubmit={handleSubmit}>
@@ -18,7 +18,7 @@ function SearchOrder() {
         id="query"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        placeholder="Search pizza.."
+        placeholder="Order status.."
         className="w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all duration-300 placeholder:text-xs placeholder:font-medium placeholder:text-stone-500 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50 sm:w-64 sm:focus:w-72"
       />
     </form>
--- pizza-hunt/src/ui/Header.jsx
+++ pizza-hunt/src/ui/Header.jsx
@@ -1,11 +1,11 @@
-import { Link } from "react-router-dom";
-import SearchOrder from "../features/order/SearchOrder";
-import Username from "../features/users/username";
+import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
+import Username from '../features/users/username';
 function Header() {
   return (
     <header className="flex items-center justify-between border-b  border-stone-300 bg-yellow-400 px-4 py-3 uppercase">
-      <Link to={"/"} className="tracking-widest">
-        Fast react Pizza Co.
+      <Link to={'/'} className="tracking-widest">
+        Pizza King Co.
       </Link>
       <SearchOrder />
       <Username />
